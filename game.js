169e94a12834a60b990abe3310b63df6fbe31595
jazzystar <jreyes60109@gmail.com>
2021-03-02 15:01:33 -0700

    Added the new files for next project: game.html game.js random.html random.js Also included the instructions from the assignment in comments @ the top

---
+++ game.js
@@ -0,0 +1,3 @@
+/*This is the javascript game file: Now on a new page called game.html, create and link to a javascript file called game.js 
+that contains a simple game that uses a similar kind of random number generator but has an interesting core game loop to play 
+(for example card games like war and blackjack, dice rolling games, number guessing games, etc. would all satisfy this requirement). */
---
+++ random.js
@@ -0,0 +1,3 @@
+/* Now use a randomization javascript function to set up a random number generator that takes a number range that a user enters 
+and gives back a random number when they click a button (for example, if I enter 1 and I00 as the range, I should get a random number 
+from 1 to 100 back in a way that is visible to me from a browser).  Indicate where a-d occur in your WDD.*/
