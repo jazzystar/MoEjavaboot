cc1a6d4a0440fe4f287c60e54896bc8d80c16aa9
jazzystar <jreyes60109@gmail.com>
2021-03-26 11:48:08 -0600

    Bootstrap CDN added (hopefully)

--- random.js
+++ random.js
@@ -1,3 +1,5 @@
 /* Now use a randomization javascript function to set up a random number generator that takes a number range that a user enters 
 and gives back a random number when they click a button (for example, if I enter 1 and I00 as the range, I should get a random number 
 from 1 to 100 back in a way that is visible to me from a browser).  Indicate where a-d occur in your WDD.*/
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
